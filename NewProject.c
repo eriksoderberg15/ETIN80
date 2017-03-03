2e182990935686c095d0eed256d2c93014c56d62
eriksoderberg15 <dat12eso@student.lu.se>
2017-03-03 12:50:25 +0100

    Add files via upload

---
+++ NewProject.c
@@ -0,0 +1,13 @@
+/*****************************************************************************
+ * NewProject.c
+ *****************************************************************************/
+
+/* Place program arguments in the following string, or delete the definition
+   if arguments will be passed from the command line. */
+const char __argv_string[] = "-abc -xyz";
+
+int main( int argc, char *argv[] )
+{
+	/* Begin adding your custom code here */
+	return 0;
+}
